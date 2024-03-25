e3eedd78de49c5ea31bfa21cd733f277ed9adde5
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-25 15:23:32 +0300

    Добавление второго компонента промежуточного слоя

---
+++ Day 1/WebHello/SecondMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace WebHello
+{
+    public class SecondMiddleware
+    {
+        private RequestDelegate _next;
+        public SecondMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await context.Response.WriteAsync($"Status code = {context.Response.StatusCode}");
+            await _next(context);
+        }
+    }
+}
