faa8964eda72d465326bf84307b50ed79c4919a8
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-25 17:51:24 +0300

    Использование UseWhen

--- Day 1/WebHello/Program.cs
+++ Day 1/WebHello/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using System.ComponentModel.Design.Serialization;
+using System.Net;
 
 namespace WebHello
 {
@@ -8,7 +9,7 @@ namespace WebHello
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(
-                new WebApplicationOptions(){ Args = args });
+                new WebApplicationOptions() { Args = args });
 
             var app = builder.Build();
 
@@ -36,13 +37,24 @@ namespace WebHello
                 await context.Response.WriteAsync($"Server time: {DateTime.Now.ToShortDateString()}"));
             });
 
-           //app.MapGet("/", () => "<h1>Hello World!</h1>");
+            //app.MapGet("/", () => "<h1>Hello World!</h1>");
 
 
             app.UseMiddleware<SecondMiddleware>();
 
+            app.UseWhen(context => context.Request.Path == "/time",
+                appBuilder => {
+                    appBuilder.Use(async (context, next) =>
+                    {
+                        await context.Response.WriteAsync("UseWhen() /time");
+                        await next();
+                    });
+                });
+
             //Third middleware (terminal)
-            app.Run(context => context.Response.WriteAsync("Hello from Run(...)"));
+            app.Run(async context => {
+                context.Response.WriteAsync("Hello from Run(...)");
+            });
 
             app.Run();
         }
