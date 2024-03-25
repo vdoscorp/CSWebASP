403ea7aa80cba5c6bd9c1a7a7fe6d8dd9bec20fd
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-25 16:37:52 +0300

    Использование метода Run c делегатом

--- Day 1/WebHello/Program.cs
+++ Day 1/WebHello/Program.cs
@@ -9,15 +9,17 @@ namespace WebHello
 
             var app = builder.Build();
 
+            app.Run(context => context.Response.WriteAsync("Hello from Run(...)"));
+
             //app.MapGet("/", () => "<h1>Hello World!</h1>");
-           /*app.Use(async (context, next) => { // First Middleware
+           app.Use(async (context, next) => { // First Middleware
                 if (context.Request.Method == HttpMethods.Get)
                 { 
                     context.Response.WriteAsync("start first Middleware");
                 }
                 await next();
                 await context.Response.WriteAsync("end first Middleware");
-            });*/
+            });
 
             app.UseMiddleware<SecondMiddleware>();
 
