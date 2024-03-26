6a319e4a1fa154dd42043c1ee11b0f4e56a3e618
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-26 11:35:55 +0300

    Использование ContentType c промежуточным слоем

--- Day 1/WebHello/Program.cs
+++ Day 1/WebHello/Program.cs
@@ -14,21 +14,85 @@ namespace WebHello
 
             var app = builder.Build();
 
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/error.html");
+            }
+
             app.UseStaticFiles(); // wwwroot
-            
-            app.Map("/date", appBuilder => {
-                appBuilder.Run(async context =>
-                await context.Response.WriteAsync($"Server date: {DateTime.Now.ToShortDateString()}"));
+            // MyStatic
+            app.UseStaticFiles(new StaticFileOptions() { 
+                FileProvider = new PhysicalFileProvider( 
+                    Path.Combine(builder.Environment.ContentRootPath, "MyStatic"))
             });
 
-            app.Map("/time", appBuilder => {
+            app.Map("/test", appBuilder => {
                 appBuilder.Run(async context =>
-                await context.Response.WriteAsync($"Server time: {DateTime.Now.ToShortTimeString()}"));
+                await context.Response.SendFileAsync("test.txt"));
             });
+
+            app.Map("/json", appBuilder => {
+                appBuilder.Run(async context =>
+                await context.Response.WriteAsJsonAsync(new { Name = "Sergey", Age = 46 }));
+            });
+
+            app.Use(async (context, next) =>
+            {
+                context.Response.ContentType = "text/html";
+                await next();
+            });
+
+            app.Use(async (context, next) => { // First Middleware
+                if (context.Request.Method == HttpMethods.Get)
+                {
+                    context.Response.WriteAsync("start first Middleware");
+                    //throw new Exception();
+                }
+                await next();
+                await context.Response.WriteAsync("end first Middleware");
+            });
+
+            app.Map("/security", appBuilder =>
+            {
+                // /security/login
+                appBuilder.Map("/login", async ab2 =>
+                    { ab2.Run(async context => await context.Response.WriteAsync("Login page.")); });
+                // /security/register
+                appBuilder.Map("/register", async ab2 =>
+                    { ab2.Run(async context => await context.Response.WriteAsync("Register page.")); });
+            });
+
+            app.Map("/date", appBuilder => {
+                appBuilder.Run(async context =>
+                await context.Response.WriteAsync($"Server time: {DateTime.Now.ToShortDateString()}"));
+            });
+
+            //app.MapGet("/", () => "<h1>Hello World!</h1>");
+
+ 
+            app.UseMiddleware<SecondMiddleware>();
+
+            /*app.UseWhen(context => context.Request.Headers.ContainsKey("Time"),
+                appBuilder => {
+                    appBuilder.Use(async (context, next) =>
+                    {
+                        await context.Response.WriteAsync("UseWhen() /time");
+                        await next();
+                    });
+                });*/
+
+            app.MapWhen(context => context.Request.Headers.ContainsKey("Time"),
+                appBuilder => {
+                    appBuilder.Use(async (context, next) =>
+                    {
+                        await context.Response.WriteAsync("MapWhen() /time");
+                        await next();
+                    });
+                });
 
             //Third middleware (terminal)
             app.Run(async context => {
-                context.Response.WriteAsync("Page not found");
+                context.Response.WriteAsync("<h1>Hello from Run(...)<h1>");
             });
 
             app.Run();
