d488777e8f43448fff6b671e3d5ae1e6b084e104
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-26 12:48:53 +0300

    Передача параметра в акшен контроллера

--- Day 2/WebMVC/Controllers/MyController.cs
+++ Day 2/WebMVC/Controllers/MyController.cs
@@ -10,6 +10,15 @@ namespace WebMVC.Controllers
             return View();
         }
 
+        public string Info()
+        {
+            int id = 0;
+            if (this.RouteData.Values["id"] != null)
+                id = int.Parse(this.RouteData.Values["id"].ToString());
+
+            return $"Request with id: {id}";
+        }
+
         //[ActionName("Hello")]
         [NonAction]
         public string Test()
