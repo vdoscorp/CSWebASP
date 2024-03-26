d8101d21a4d3036b3e7e87b14609ab1e16a9b670
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-26 21:56:35 +0300

    Добавлены модель и контроллер курсов

---
+++ Day 2/WebMVC/Controllers/CourseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Json(Course.All);
+        }
+    }
+}
---
+++ Day 2/WebMVC/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿namespace WebMVC.Models
+{
+    public class Course
+    {
+        public static IList<Course> All { get; set; }
+
+        static Course()
+        {
+            All = new List<Course>()
+            {
+                new Course() { Id = 1, Title = "C# 12", Duration = 40, Description = "C# Intro" },
+                new Course() { Id = 2, Title = "C# Client-Server", Duration = 40, Description = "Creating service with c#" },
+                new Course() { Id = 3, Title = "JavaScript", Duration = 24, Description = "JavaScript Intro" },
+                new Course() { Id = 4, Title = "Java 1", Duration = 40, Description = "Java Intro" }
+            };
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public string Description { get; set; }
+    }
+}
