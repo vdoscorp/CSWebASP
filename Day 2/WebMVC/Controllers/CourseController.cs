35e1742b49128c0a715fd606e69093aa54640b90
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-26 22:15:09 +0300

    Использование экшен контроллера с атрибутом Route

--- Day 2/WebMVC/Controllers/CourseController.cs
+++ Day 2/WebMVC/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using WebMVC.Models;
 
 namespace WebMVC.Controllers
@@ -14,6 +15,12 @@ namespace WebMVC.Controllers
         {
             return Json(Course.All.Where(c => c.Title.Contains(search, StringComparison.OrdinalIgnoreCase)
             || c.Description.Contains(search, StringComparison.OrdinalIgnoreCase)));
+        }
+
+        [Route("course/{id:int}")]
+        public IActionResult Details(int id)
+        {
+            return Json(Course.All.Where(c => id == c.Id).SingleOrDefault());
         }
     }
 }
