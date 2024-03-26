2e4f37cdf19e2a528616d232f6d8cca29e4eb689
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-26 17:53:27 +0300

    Лабораторная работа 3 (MVC контроллеры и фильтры)

---
+++ Day 2/LabMVC/Controllers/MyController.cs
@@ -0,0 +1,41 @@
+﻿using LabMVC.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace LabMVC.Controllers
+{
+    [MyActionFilter]
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Params(string name, int age)
+        {
+            return $"Data from query name: {name} age: {age}";
+        }
+
+        [ActionName("Json")]
+        public IActionResult JsonAction(Models.Person p)
+        {
+            return this.Json(p);
+        }
+
+        public string Info()
+        {
+            int id = 0;
+            if (this.RouteData.Values["id"] != null)
+                id = int.Parse(this.RouteData.Values["id"].ToString());
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Request with id: {id}");
+
+            foreach (var header in HttpContext.Request.Headers)
+                sb.AppendLine($"{header.Key} : {header.Value}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Day 2/LabMVC/Filters/MyActionFilterAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LabMVC.Filters
+{
+    public class MyActionFilterAttribute : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine("ActionExecuting");
+            if (context.ActionArguments.Any())
+            {
+                Console.WriteLine("Params:");
+                foreach (var arg in context.ActionArguments)
+                {
+                    Console.WriteLine($"{arg.Key} : {arg.Value}");
+                }
+            }
+            var list = context.RouteData.Values.Where(kv => kv.Key != "action" && kv.Key != "controller").ToList();
+            if (list.Any())
+            {
+                Console.WriteLine("RouteData:");
+                foreach (var kv in context.RouteData.Values.Where(kv => kv.Key != "action" && kv.Key != "controller"))
+                {
+                    Console.WriteLine($"{kv.Key} : {kv.Value}");
+                }
+            }
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine($"ActionExecuted Result: {context.Result}");
+        }
+    }
+}
