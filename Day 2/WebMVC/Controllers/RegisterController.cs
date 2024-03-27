164cc45ef61f7a029f6b6b7c76f4f3f471aece74
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-27 15:42:51 +0300

    Добавлена форма регистрации

---
+++ Day 2/WebMVC/Controllers/RegisterController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Registration());
+        }
+    }
+}
---
+++ Day 2/WebMVC/Models/Registration.cs
@@ -0,0 +1,12 @@
+﻿namespace WebMVC.Models
+{
+    public class Registration
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string ConfilmPassword { get; set; }
+        public DateOnly DOB { get; set; }
+        public string Email { get; set; }
+        public int Weight { get; set; }
+    }
+}
