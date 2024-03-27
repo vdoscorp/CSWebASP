dd5a07e3a256a0bac92eefada6eb98af1b156514
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-27 13:17:30 +0300

    Передача данных через форму средствами MVC

---
+++ Day 2/WebMVC/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new HelloVM());
+        }
+
+        [HttpPost]
+        public IActionResult SayHello(HelloVM m)
+        {
+            return View("Index", m);
+        }
+    }
+}
---
+++ Day 2/WebMVC/Models/HelloVM.cs
@@ -0,0 +1,10 @@
+﻿namespace WebMVC.Models
+{
+    public class HelloVM
+    {
+        public string UserName { get; set; }
+        public string Hello {
+            get => string.IsNullOrWhiteSpace(UserName)? "Привет!" : $"Привет, {UserName}!";
+        }
+    }
+}
