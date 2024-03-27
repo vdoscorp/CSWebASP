77e96b6a093f89195b0f8b7f153808497c37e827
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-27 15:08:28 +0300

    Использование атрибута Display для изменения отображаемого имени

--- Day 2/WebMVC/Models/HelloVM.cs
+++ Day 2/WebMVC/Models/HelloVM.cs
@@ -1,7 +1,11 @@
-﻿namespace WebMVC.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebMVC.Models
 {
     public class HelloVM
     {
+        [Display(Name = "User name")]
         public string UserName { get; set; }
         public string Hello {
             get => string.IsNullOrWhiteSpace(UserName)? "Привет!" : $"Привет, {UserName}!";
