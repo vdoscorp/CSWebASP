2f6d09ae44f22df116484578dc952e53fcc249cb
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-28 23:42:30 +0300

    Добавлены недостающие изменения CourseController'а проекта WebMVС для тестирования зависимостей

--- Day 2/WebMVC/Controllers/CourseController.cs
+++ Day 2/WebMVC/Controllers/CourseController.cs
@@ -6,30 +6,37 @@ namespace WebMVC.Controllers
 {
     public class CourseController : Controller
     {
+        private IEnumerable<Course> _courses;
+
+        public CourseController(ICourseData cd)
+        {
+            this._courses = cd.All;
+        }
+
         public IActionResult Index()
         {
-            return Json(Course.All);
+            return Json(_courses);
         }
 
         [Route("courses")]
         public IActionResult List()
         {
-            ViewData["CoursesCount"] = Course.All.Count();
-            return View(Course.All);
+            ViewData["CoursesCount"] = _courses.Count();
+            return View(_courses);
         }
 
         //[Route("coursesearch/{search}")]
         [Route("search/{search}")] // higher priority then MapControllerRoute
         public IActionResult Search(string search)
         {
-            return Json(Course.All.Where(c => c.Title.Contains(search, StringComparison.OrdinalIgnoreCase)
+            return Json(_courses.Where(c => c.Title.Contains(search, StringComparison.OrdinalIgnoreCase)
             || c.Description.Contains(search, StringComparison.OrdinalIgnoreCase)));
         }
 
         [Route("course/{id:int}")]
         public IActionResult Details(int id)
         {
-            return Json(Course.All.Where(c => id == c.Id).SingleOrDefault());
+            return Json(_courses.Where(c => id == c.Id).SingleOrDefault());
         }
     }
 }
