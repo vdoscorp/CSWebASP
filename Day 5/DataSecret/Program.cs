d883ebda85c176faf5163180bb620b4474c0347e
vdoscorp <7358852+vdoscorp@users.noreply.github.com>
2024-03-29 16:53:49 +0300

    Добавлено приложение вывода секретов пользователя

---
+++ Day 5/DataSecret/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .UseEnvironment("Development")
+    .Build();
+IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+string value_1 = config["Key_ONE"];
+string value_2 = config["Key_TWO"];
+
+Console.WriteLine($"KeyOne = {value_1}");
+Console.WriteLine($"KeyTwo = {value_2}");
+
+//await host.RunAsync();
